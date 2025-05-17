8d7583d166eefc3b74342c3ec61c392417fdeb5f
MaxDzioban <149308245+MaxDzioban@users.noreply.github.com>
2025-05-17 19:48:01 +0300

    Update final_server.js

--- server/final_server.js
+++ server/final_server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // --- MongoDB ---
 const uri = process.env.MONGO_URI;
